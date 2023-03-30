0bac98b0b6de1653b27aa5c78b605287a9310cb6
Andrew <abrusoe1@gmail.com>
2023-03-30 18:44:21 -0400

    finished QuestionAPIController

--- StudyBuddy API/src/main/java/co/grandcircus/StudyBuddy/QuestionAPIController.java
+++ StudyBuddy API/src/main/java/co/grandcircus/StudyBuddy/QuestionAPIController.java
@@ -1,7 +1,10 @@
 package co.grandcircus.StudyBuddy;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +12,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@CrossOrigin
 public class QuestionAPIController {
 
 	@Autowired
 	private StudyBuddyRepository repo;
 	
+	@RequestMapping("/")
+	public String home() {
+		return "redirect:questions";
+	}
+	
 	// C(R)UD -- Read All
-//	@GetMapping("/questions")
+	@GetMapping("/questions")
+	public List<StudyBuddy> readAll(@RequestParam String question) {
+		return repo.findAll();
+	}
 	
 	//C(R)UD -- Read One
 	@GetMapping("/questions/{id}")

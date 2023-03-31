ad1e5f66603260806a12dc3f07f3be02e02dd26d
chloedgood <115897586+chloedgood@users.noreply.github.com>
2023-03-30 21:51:40 -0400

    adding question changes and question list changes

--- StudyBuddy/src/app/question-list/question-list.component.ts
+++ StudyBuddy/src/app/question-list/question-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StudyBuddy } from '../study-buddy';
+import { StudyBuddyService } from '../study-buddy.service';
 
 @Component({
   selector: 'app-question-list',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./question-list.component.css']
 })
 export class QuestionListComponent {
+  questions: StudyBuddy[] =[];
+  newQuestion:StudyBuddy = ({} as any) as StudyBuddy; 
+
+  constructor(private StudyBuddyAPI: StudyBuddyService){}
+
+  ngOnInit(){
+    this.loadQuestions();
+  }
+
+  loadQuestions():void{
+    this.StudyBuddyAPI.getAllQuestions().subscribe(
+      (result) => {
+        this.questions = result;
+      }
+    )
+  }
+
+  deleteQuestion(id:number):void{
+    this.StudyBuddyAPI.deleteQuestion(id).subscribe(
+      (result)=>{
+        console.log('Item deleted at ' + id);
+        this.loadQuestions();
+      }
+    );
+  }
+
+    addQuestions(newQuestion:StudyBuddy){
+      this.questions.push(newQuestion);   // come back to this later
+      this.loadQuestions();
+    }
 
 }
